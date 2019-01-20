f32b53952912a826b13fb553b123b91286c3d99d
tony201m <35059196+tony201m@users.noreply.github.com>
2019-01-20 18:44:24 +0700

    Update TONY.cs

--- TONY.cs
+++ TONY.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System; 
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace kontolasu
+namespace TONY
 {
     public partial class Form1 : Form
     {
